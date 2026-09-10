8c3311e1ef28ab2d427a741fc29d5653e2f6edab
muhammadrasyidridho25-hash <muhammadrasyidridho25@mhs.politala.ac.id>
2026-09-10 10:03:21 +0700

    first commit

---
+++ src/Exmud.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Exmud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner masuk = new Scanner (System.in);
+        int nilaia;
+        System.out.println("MASUKKAN NLAI A");
+        nilaia = masuk.nextInt();
+        System.out.println("MASUKKAN NILAI B = ");
+        int nilaib;
+        nilaib = masuk.nextInt();
+        System.out.println("MASUKKAN NILAI C = ");
+        int nilaic;
+        nilaic = masuk.nextInt();
+        System.out.println("MASUKKAN NILAI D = ");
+        int nilaid;
+        nilaid = masuk.nextInt();
+        System.out.println("MASUKKAN NILAI E = ");
+        int nilaie;
+        nilaie = masuk.nextInt();
+       
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
